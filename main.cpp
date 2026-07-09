d4ef3127df500f38bc75fb5b57df8d4d7586290c
Vaishnavi Singh <vaishnavi.singhhbr@gmail.com>
2026-07-09 09:57:43 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,30 +23,41 @@ int GenerateRandomNumber()
 int main()
 {
     int guess;
-    ShowTitle();
     srand(time(0));
-    int num = GenerateRandomNumber();
-    int attempts = 0;
-    while (true)
+    char choice = 'Y';
+    while(choice == 'Y' || choice == 'y')
     {
-    TakeGuess(guess);
-    attempts++;
-    if (guess < num)
-    {
-        cout<<"Too low!\n";
-    }
-    else if (guess > num)
-    {
-        cout<<"Too high!\n";
-    }
+        ShowTitle();
+        int num = GenerateRandomNumber();
+        int attempts = 0;
+        while (true)
+            {
+            TakeGuess(guess);
+            attempts++;
+            if (guess < num)
+            {
+                cout<<"Too low!\n";
+            }
+            else if (guess > num)
+            {
+                cout<<"Too high!\n";
+            }
 
-    else
-    {
-        cout<<"Yes! You've guessed it right!\n";
-        cout<<"You guessed the number in "<<attempts<<" attempt(s)!\n";
-        break;
-
+            else
+            {
+                cout<<"Yes! You've guessed it right!\n";
+                cout<<"You guessed the number in "<<attempts<<" attempt(s)!\n";
+                break;
+            }
+        }
+        cout<<"Do you want to play again? (y/n)";
+        cin>>choice;
+        cout<<"\n";
     }
-    }
-  return 0;
+    cout<<"Thanks for playing! \n";
+    cout<<"Have a nice day!\n";
+    
+    return 0;
 }
+            
+                
